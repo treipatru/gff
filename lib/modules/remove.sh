ac7cd3e3be69f0fd931ae5f113e3772ba9f60c87
treipatru <andrei@planet34.org>
2022-08-28 11:01:41 +0200

    feat: add git remove functionality

---
+++ lib/modules/remove.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+. "${GFZ_FOLDER}/finder.sh"
+
+gfz_remove () {
+    local GFZ_ITEMS
+    GFZ_ITEMS=$(git ls-tree --full-tree -r --name-only HEAD)
+
+    FZF_DEFAULT_OPTS+="\
+        --multi
+        --header 'Git rm files' \
+        --preview 'bat \
+            --style numbers,changes \
+            --color=always \
+            {1} \
+            | head -500' \
+        --preview-window up,border-bottom,80%
+        --bind 'ctrl-o:execute-silent(code --goto {1})' \
+    "
+
+    echo "$GFZ_ITEMS" \
+        | gfz_finder \
+        | xargs git rm
+}
