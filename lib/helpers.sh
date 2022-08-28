b8a778f608ed728df32bb234e8e42c4d4d64a2e6
treipatru <andrei@planet34.org>
2022-08-28 10:33:31 +0200

    refactor: centralize error management

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -1,18 +1,52 @@
 #!/usr/bin/env bash
 
+gfz_emit_error () {
+    local ERROR_CODE
+    local ERROR_STR
+    ERROR_CODE=$1
+
+    case $ERROR_CODE in
+        "1")
+            ERROR_STR="Git is not installed"
+            ;;
+        "2")
+            ERROR_STR="FZF is not installed"
+            ;;
+        "3")
+            ERROR_STR="Bat is not installed"
+            ;;
+        "4")
+            ERROR_STR="RG is not installed"
+            ;;
+        "5")
+            ERROR_STR="Not in git repo"
+            ;;
+        "6")
+            ERROR_STR="Nothing to commit, staging area empty"
+            ;;
+        "7")
+            ERROR_STR="Nothing to add, working tree clean"
+            ;;
+        *)
+            ERROR_STR="GFZ encountered an error"
+    esac
+
+    echo "» ${ERROR_STR}"
+    exit 1
+}
+
 gfz_check_environment () {
     # Check for required software
-    command -v git >/dev/null 2>&1 || { echo >&2 "» Git is not installed. Aborting."; exit 1; }
-    command -v fzf >/dev/null 2>&1 || { echo >&2 "» FZF is not installed. Aborting."; exit 1; }
-    command -v bat >/dev/null 2>&1 || { echo >&2 "» Bat is not installed. Aborting."; exit 1; }
-    command -v rg  >/dev/null 2>&1 || { echo >&2 "» RG is not installed. Aborting."; exit 1; }
+    command -v git >/dev/null 2>&1 || gfz_emit_error 1
+    command -v fzf >/dev/null 2>&1 || gfz_emit_error 2
+    command -v bat >/dev/null 2>&1 || gfz_emit_error 3
+    command -v rg  >/dev/null 2>&1 || gfz_emit_error 4
 
     # Check for Git repository
     local DIR_CODE
     DIR_CODE=$(git -C . rev-parse 2>/dev/null; echo $?)
     if [ "$DIR_CODE" -eq "128" ]; then
-       echo " » Not in git repo";
-       exit 1;
+       gfz_emit_error 5
     fi
 
     # Finally apply some FZF options if the user has none
@@ -53,8 +87,7 @@ gfz_commit () {
     STATUS=$(git diff --name-only --cached)
 
     if [[ -z "$STATUS" ]]; then
-        echo "» No changes staged. Aborting."
-        exit
+       gfz_emit_error 6
     else
         git commit --verbose
     fi
--- lib/modules/add.sh
+++ lib/modules/add.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 . "${GFZ_FOLDER}/finder.sh"
+. "${GFZ_FOLDER}/helpers.sh"
 
 gfz_add () {
     local GFZ_ITEMS
     GFZ_ITEMS=$(git ls-files -d -m -o --exclude-standard --full-name )
 
     if [ -z "$GFZ_ITEMS" ]; then
-       echo "Nothing to add, working tree clean";
-       exit
+        gfz_emit_error 7
     fi
 
     FZF_DEFAULT_OPTS+=" \
