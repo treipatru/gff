f7521ffdcd9e7b3719c6a055570ce5af05e0f41c
treipatru <andrei@planet34.org>
2022-08-31 20:18:28 +0200

    fix: exist silently from branch creation on 0 length input

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -142,6 +142,11 @@ gfz_create_or_switch_branch () {
     local BRANCH
     BRANCH=$1
 
+    # If no input just exit silently
+    if [[ -z $BRANCH ]]; then
+        exit 0
+    fi
+
     if [[ -z $(git branch --list "${BRANCH}") ]]; then
         git branch "$BRANCH"
         git switch "$BRANCH"
