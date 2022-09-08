dd179b30adfc1a28d216e394668b4cd3a5461918
treipatru <andrei@planet34.org>
2022-09-08 08:57:49 +0200

    refactor: improve chunk generation method
    * Replace awk and sed with string expansion
    * More informative var names

--- lib/modules/chunk.sh
+++ lib/modules/chunk.sh
@@ -3,62 +3,54 @@
 . "${GFZ_FOLDER}/helpers.sh"
 
 gfz_chunk () {
-    local GFZ_DIFF_FOLDER GFZ_ITEMS FZF_ITEMS_FILE FZF_SELECTION
-    GFZ_ITEMS=$(git diff --name-only --diff-filter=MA)
-    GFZ_DIFF_FOLDER="/tmp/gfz_diff"
-    FZF_ITEMS_FILE="${GFZ_DIFF_FOLDER}/FZF_ITEMS"
+    local ROOT_DIFF_FOLDER GIT_FILES FZF_ITEMS_FILE FZF_SELECTION
+    GIT_FILES=$(git diff --name-only --diff-filter=MA)
+    ROOT_DIFF_FOLDER="/tmp/gfz_diff"
+    FZF_ITEMS_FILE="${ROOT_DIFF_FOLDER}/FZF_ITEMS"
 
-    if [ -z "$GFZ_ITEMS" ]; then
+    if [ -z "$GIT_FILES" ]; then
         gfz_emit_error 7
     fi
 
-    # 1 create a diff folder if it doesn't exist
-    if [ ! -d $GFZ_DIFF_FOLDER ]; then
-        mkdir $GFZ_DIFF_FOLDER
-    else
-        rm -rf $GFZ_DIFF_FOLDER
-        mkdir $GFZ_DIFF_FOLDER
-    fi
+    rm -rf $ROOT_DIFF_FOLDER
+    mkdir $ROOT_DIFF_FOLDER
     touch $FZF_ITEMS_FILE
 
-    # 2 iterate through status items and
-    echo "$GFZ_ITEMS" | while IFS= read -r LINE; do
-        local FOLDER FILE
-        FOLDER="${GFZ_DIFF_FOLDER}/$(echo "$LINE" | sed -r 's/[/. ]+/-/g')"
-        FILE=$(basename "$LINE")
+    echo "$GIT_FILES" | while IFS= read -r GIT_FILE; do
+        local DIFF_FILE
+        DIFF_FILE="${ROOT_DIFF_FOLDER}/${GIT_FILE//[\/\. ]}/${GIT_FILE##*/}"
 
-        # Generate a folder for each file
-        mkdir "$FOLDER"
-        # Generate a main diff file for each file
-        git diff-files --patch --diff-algorithm=minimal --output="$FOLDER"/"$FILE" "$LINE"
+        # Make directory for each GIT_FILE and generate a main diff
+        mkdir "${DIFF_FILE%\/*}"
+        git diff-files --patch --diff-algorithm=minimal --output="$DIFF_FILE" "$GIT_FILE"
+
         # Generate patch files for each patch
+        rg '^@@.*@@' --line-number "$DIFF_FILE" | while IFS= read -r CHUNK_HEADER; do
+            local CHUNK_FILE NEXT_LINE CHUNK_INFO
 
-        rg '^@@.*@@' --line-number "$FOLDER"/"$FILE" | while IFS= read -r LINE2; do
-            local CHUNK_FILE CHUNK_LINE NEXT_LINE DIFF_REGION
-            # Generate a unique file identifier from chunk meta and append that to $FILE
-            # 5:@@ -1,20 +1,50 @@ > 120150
-            CHUNK_FILE="${FOLDER}/${FILE}_CHUNK_$(echo "$LINE2" | awk '{print $2,$3}' | sed 's/[+, -]//g')"
-            # Get chunk start line
-            CHUNK_LINE=${LINE2%%:*}
-            # Get first line of chunk content
-            NEXT_LINE=$(($CHUNK_LINE + 1))
+            # Generate a unique file identifier from chunk data
+            CHUNK_FILE="${DIFF_FILE}_${CHUNK_HEADER//[^0-9]}.patch"
 
-            echo "${LINE} ${CHUNK_FILE} ${LINE2}" >> $FZF_ITEMS_FILE
+            # Get first line of chunk content by adding 1 to the rg line
+            NEXT_LINE=$((${CHUNK_HEADER%%:*} + 1))
 
-            # Write file header
-            # Git output is always 4 lines
-            head -4 "$FOLDER"/"$FILE" > "$CHUNK_FILE"
+            # Write patch file header. Git header and chunk data
+            head -4 "$DIFF_FILE" > "$CHUNK_FILE"
+            echo "${CHUNK_HEADER#*:}" >> "$CHUNK_FILE"
 
-            # Write the diff header first
-            echo "${LINE2#*:}" >> "$CHUNK_FILE"
             # Then the rest before next diff header
-            tail -n +"$NEXT_LINE" "$FOLDER"/"$FILE" | while IFS= read -r LINE3; do
+            tail -n +"$NEXT_LINE" "$DIFF_FILE" | while IFS= read -r LINE3; do
                 if [[  "$LINE3" == *@@ || $LINE3 ==  @@* ]]; then
                     break
                 else
                     echo "$LINE3" >> "$CHUNK_FILE"
                 fi
             done
+
+            # Make chunk available to FZF search
+            CHUNK_INFO=${CHUNK_HEADER#*@@}
+            CHUNK_INFO=${CHUNK_INFO%%@@*}
+            echo "${GIT_FILE} ${CHUNK_FILE} ${CHUNK_INFO}" >> $FZF_ITEMS_FILE
         done
     done
 
